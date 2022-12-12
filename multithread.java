2d09d6927669c685514aaf0e9c1731f0095cfc47
walter083 <115606559+walter083@users.noreply.github.com>
2022-12-12 13:13:39 +0530

    Update multithread.java

--- multithread.java
+++ multithread.java
@@ -1,1 +1,54 @@
 
+class GoodMorning extends Thread {
+    synchronized public void run() {
+        try {
+            int i=0;
+            while (i<5) {
+                sleep(1000);
+                System.out.println("Good morning ");
+                i++;
+            }
+        } catch (Exception e) {
+        }
+    }
+}
+ 
+class Hello extends Thread {
+    synchronized public void run() {
+        try {
+            int i=0;
+            while (i<5) {
+                sleep(2000);
+                System.out.println("hello");
+                i++;
+            }
+        } catch (Exception e) {
+        }
+    }
+}
+ 
+class Welcome extends Thread {
+    synchronized public void run() {
+        try {
+            int i=0;
+            while (i<5) {
+                sleep(3000);
+                System.out.println("welcome");
+                i++;
+            }
+        } catch (Exception e) {
+        }
+    }
+}
+ 
+class MultithreadDemo {
+    public static void main(String args[]) {
+        GoodMorning t1 = new GoodMorning();
+        Hello t2 = new Hello();
+        Welcome t3 = new Welcome();
+        t1.start();
+        t2.start();
+        t3.start();
+     
+    }
+}
